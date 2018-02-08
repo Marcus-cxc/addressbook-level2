b269e4643f400388acdd05cbf202ca3e783208a4
Marcus Chen <Marcus_cxc@outlook.com>
2018-02-08 18:44:20 +0800

    Address class now store block, street, unit and postal separately

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -2,6 +2,9 @@ package seedu.addressbook.data.person;
 
 import seedu.addressbook.data.exception.IllegalValueException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
@@ -9,8 +12,14 @@ import seedu.addressbook.data.exception.IllegalValueException;
 public class Address {
 
     public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS =
+            "Person addresses has to be entered by a/ BLOCK, STREET, UNIT, POSTAL CODE";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
+
+    private static String block;
+    private static String street;
+    private static String unit;
+    private static String postal;
 
     public final String value;
     private boolean isPrivate;
@@ -27,18 +36,46 @@ public class Address {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
         this.value = trimmedAddress;
+
+
     }
 
     /**
      * Returns true if a given string is a valid person address.
      */
     public static boolean isValidAddress(String test) {
-        return test.matches(ADDRESS_VALIDATION_REGEX);
+        ArrayList<String> separatedFormat = splitByComma(test);
+        if(separatedFormat.size() == 4) {
+            block = separatedFormat.get(0).trim();
+            street = separatedFormat.get(1).trim();
+            unit = separatedFormat.get(2).trim();
+            postal = separatedFormat.get(3).trim();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
     }
+
+
+    /**
+     * Splits a source string into the list of substrings that were separated by comma.
+     *
+     * @param toSplit source string
+     * @return split by whitespace
+     */
+    private static ArrayList<String> splitByComma(String toSplit) {
+        return new ArrayList<>(Arrays.asList(toSplit.trim().split(",")));
+    }
+
+
+
 
     @Override
     public String toString() {
-        return value;
+        return block + ", " + street + ", " + unit + ", " + postal;
     }
 
     @Override
