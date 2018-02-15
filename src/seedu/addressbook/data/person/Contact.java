a7966ba43abd4f8c06b038a3dd9a63b2e5feaa4f
Marcus Chen <Marcus_cxc@outlook.com>
2018-02-15 12:57:39 +0800

    Create a Contact class, Parent class of Address,Email,Phone

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,37 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Parent class of Address, Email and Phone
+ *
+ */
+public class Contact {
+
+    public final String value;
+    private boolean isPrivate;
+
+    public Contact(String value,boolean isPrivate){
+        this.value = value.trim();
+        this.isPrivate = isPrivate;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Phone // instanceof handles nulls
+                && this.value.equals(((Phone) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
